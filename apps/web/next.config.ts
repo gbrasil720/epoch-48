490d5fea78006e2c0538aefe7325f2776562f2e2
Guilherme Brasil <dev.guilhermebrasil@gmail.com>
2026-07-09 10:38:53 -0400

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@epoch-48/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
